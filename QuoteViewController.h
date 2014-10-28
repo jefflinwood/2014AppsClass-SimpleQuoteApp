066e244378c1d16f194212313ed6b918cf6d61b9
Jeffrey Linwood <jlinwood@gmail.com>
2014-10-28 10:53:57 -0500

    Custom view controller class for the home screen

---
+++ QuoteViewController.h
@@ -0,0 +1,13 @@
+//
+//  QuoteViewController.h
+//  SimpleQuoteApp
+//
+//  Created by Jeffrey Linwood on 10/23/14.
+//  Copyright (c) 2014 Jeff Linwood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuoteViewController : UIViewController
+
+@end
